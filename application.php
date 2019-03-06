2dab27a4309f279aa5947546e66f3c91f380362f
Aaishah13 <aishatakinola17@gmail.com>
2019-03-06 15:21:29 +0100

    portal

---
+++ application.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    
+</head>
+<body>
+    <div class="container-fluid bg h-100">
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 login">
+           <div class="row">
+             <div class="col text-center">
+             <h4 class="form-header">Login to your dashboard</h4>
+             <img src="assets/img/lock.jpg" alt="">
+             </div>
+           </div>
+           <div class="row">
+             <div class="col-md-2"></div>
+             <div class="col-md-8">
+             <form class="myform">
+               <div class="form-group">
+                <label for="username">Registration Number</label>
+                <input type="text" class="form-control" placeholder="regnumber"><i class="fa fa-user errspan"></i>
+              </div>
+            <div class="form-group">
+            <label for="password">Password</label>
+            <input type="text" class="form-control" placeholder="password"><i class="fa fa-lock errspan"></i>
+             </div>
+             <div class="checkbox">
+            <label for=""> &nbsp; <input type="checkbox"> Remember me?</label>
+             </div>
+            <div>
+              <input type="submit" class="btn btn-success" value="Login">
+            </div>
+              </form>
+             </div>
+             <div class="col-md-2"></div>
+           </div>
+        </div>
+        <div class="col-md-3"></div>
+     </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Codesquad School of Tech</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+   <nav class="navbar navbar-expand-lg navbar" id="banner">
+      <a class="navbar-brand" href="#" id="navbaritem">
+        <img src="assets/img/code.png" alt="school logo">CodeSquad School of Tech</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+   <div class="collapse navbar-collapse navbar-right" id="navbarNavDropdown">
+     <div class="navbar-nav">
+        <a class="nav-link" href="#" id="navitem">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#" id="navitem">About</a>
+        <a class="nav-link" href="#" id="navitem">Admissions</a>
+        <a class="nav-link" href="#" id="navitem">Students</a>
+        <a class="nav-link" href="#" id="navitem">Contact us</a>
+     </div>
+  </div>
+</nav>
+ <!-- ************** Navbar code ends here ************** -->
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <img class="my-image" src="assets/img/univ.jpg" alt="">
+        </div>
+     </div>
+    <div class="row mt-4">
+        <div class="col-12">
+        <div class="caption">
+            <h3 class="text-center my-text">CodeSquad School of Tech has built a legacy of <br>
+             <span class="my-text1">Academic Excellence</span></h3>
+        </div>
+        </div>
+     </div>
+    <div class="row mx-5">
+        <div class="col-md-4 image1">
+            <img src="assets/img/school2.jpg" alt="" class="img-thumbnail img1">
+                <div class="caption">
+                    <h5>Prospective Postgraduate students</h5>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quae nostrum magni repellendus autem rerum provident asperiores natus repellat, 
+                        excepturi vel enim, consequatur possimus doloremque aut molestiae voluptatibus. Provident, iusto.</p>
+                    <a href="application.php"><button>Click Here</button></a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/school3.jpg" alt="" class="img-thumbnail img1">
+                <div class="caption">
+                    <h5>Prospective Undergraduate students</h5>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quae nostrum magni repellendus autem rerum provident asperiores natus repellat, 
+                        excepturi vel enim, consequatur possimus doloremque aut molestiae voluptatibus. Provident, iusto.</p>
+                    <a href="application.php"><button>Click Here</button></a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/school4.jpg" alt="" class="img-thumbnail img1">
+                <div class="caption">
+                     <h5>Academic Calendars</h5>
+                     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quae nostrum magni repellendus autem rerum provident asperiores natus repellat, 
+                        excepturi vel enim, consequatur possimus doloremque aut molestiae voluptatibus. Provident, iusto.</p>
+                     <a href=""><button>Click Here</button></a>
+            </div>
+        </div>
+</div>
+
+    <footer>
+       <div class="row my-footer mt-4">
+           <div class="col-md-6 text-center mt-2">
+               <h4>Welcome to CodeSquad School of Technology</h4>
+           </div>
+           <div class="col-md-6 text-right">
+               <p>All rights reserved</p>
+           </div>
+       </div>
+    </footer>
+</div>
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
